c37dd8eed56e63222a0efe07a34eafc91d073a6e
Louisaastrid <60742071+Louisaastrid@users.noreply.github.com>
2020-05-26 17:08:14 +0200

    html webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,31 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const htmlWebpackTemplate = require("html-webpack-template");
 
 module.exports = {
+  mode: "production",
   entry: "./src/index.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
   },
   plugins: [
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: "Recipes Application",
+      title: "Recipes application",
       meta: { viewport: "width=device-width, initial-scale=1.0" },
+      inject: true,
+      template: htmlWebpackTemplate,
+      links: [
+        "https://fonts.googleapis.com/css2?family=Roboto:wght@100;400&display=swap",
+      ],
     }),
   ],
   module: {
     rules: [
       {
-        test: /\.css$/,
+        test: /.css$/,
         use: ["style-loader", "css-loader"],
       },
     ],
